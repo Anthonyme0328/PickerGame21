7c07194e8615416985a4cdb3c773004a8db2dcf8
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2021-05-20 14:49:28 -0700

    added in function to generate random nums for winner condition WIP

--- script.js
+++ script.js
@@ -2,4 +2,28 @@ function myFunction() {
   alert('i am wokring')
 }
 
-let options = ["one", "two", "three", "four", "five"]
+// let option1 = Math.floor(Math.random() * (5 - 1) + 1)
+// let option2 = Math.floor(Math.random() * (5 - 1) + 1)
+// let option3 = Math.floor(Math.random() * (5 - 1) + 1)
+// let option4 = Math.floor(Math.random() * (5 - 1) + 1)
+// let option5 = Math.floor(Math.random() * (5 - 1) + 1)
+
+// function beginGame() {
+//   option1 = Math.floor(Math.random() * (5 - 1) + 1);
+//   option2 = Math.floor(Math.random() * (5 - 1) + 1);
+//   option3 = Math.floor(Math.random() * (5 - 1) + 1);
+//   option4 = Math.floor(Math.random() * (5 - 1) + 1);
+//   option5 = Math.floor(Math.random() * (5 - 1) + 1)
+// }
+
+let choices = [1, 2, 3, 4, 5]
+
+let gen = function numberGeneration() {
+  Math.floor(Math.random() * (5 - 1) + 1)
+}
+
+function picker (){
+ console.log(choices.map(gen))
+}
+
+picker();
