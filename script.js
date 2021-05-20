4178909ee4ca8b42aba45914bb6a141878a2cf43
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2021-05-20 15:32:14 -0700

    added in comments for references while dev

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
+
+// dummy function for testing to see if buttons are working needs to be removed after testing
 function myFunction() {
   alert('i am wokring')
 }
 
-// let option1 = Math.floor(Math.random() * (5 - 1) + 1)
-// let option2 = Math.floor(Math.random() * (5 - 1) + 1)
-// let option3 = Math.floor(Math.random() * (5 - 1) + 1)
-// let option4 = Math.floor(Math.random() * (5 - 1) + 1)
-// let option5 = Math.floor(Math.random() * (5 - 1) + 1)
-
-// function beginGame() {
-//   option1 = Math.floor(Math.random() * (5 - 1) + 1);
-//   option2 = Math.floor(Math.random() * (5 - 1) + 1);
-//   option3 = Math.floor(Math.random() * (5 - 1) + 1);
-//   option4 = Math.floor(Math.random() * (5 - 1) + 1);
-//   option5 = Math.floor(Math.random() * (5 - 1) + 1)
-// }
-
+// array of choices that will be compared to the random number generated 
 let choices = [1, 2, 3, 4, 5]
 
+// decalred variable to allow of comparisons in multiple functions
 let answer;
 
+// function that generates and console logs a number between 1 and 5
 function numberGeneration() {
  answer = Math.floor(Math.random() * (5) + 1)
  console.log(answer)
  return answer
 }
 
-
-
-
-
+// function to compare number gmerated and number selected
+// currently not working more than once and hard code value for testing
 function picker (){
  if (answer === 1){
    console.log('winner')
@@ -37,7 +26,7 @@ function picker (){
   
 }
 
-
-
+// calling the functions
+// should be changed to recursivly call the fucntions where appropriate
 picker();
 numberGeneration();
